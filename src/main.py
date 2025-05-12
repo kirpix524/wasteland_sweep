4d91449fb6aac41ab76c85648e1ba97ffccbe88d
Николай <kirpix524@gmail.com>
2025-05-12 16:56:56 +0400

    fixed audio channels

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def main():
     register_states(state_manager)
     state_manager.change_state("menu")
 
-    pygame.mixer.set_num_channels(64)
+    pygame.mixer.set_num_channels(32)
 
     # Main loop
     running = True
